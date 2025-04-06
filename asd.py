32ca50bdb182e38eefca0b12d7a844ecdc6e5bf8
Adonis95 <Sober4202020@outlook.com>
2025-04-05 22:09:41 -0600

    setting up .env and .gitignore

--- asd.py
+++ asd.py
@@ -1,8 +1,18 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+host = os.getenv("db_host")
+user = os.getenv("db_user")
+password = os.getenv("db_password")
+
 
 mydb = mysql.connector.connect(
-    host = '',
-    user = '',
-    password = ''
+    host = host,
+    user = user,
+    password = password
 )
 
+print(mydb)
